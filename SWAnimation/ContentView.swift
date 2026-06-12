cf93b51b54a05b50a260d12d55ad8a081d9c580a
wannabemountainking <kdy0096@gmail.com>
2026-06-12 17:30:55 +0900

    Initial Commit

---
+++ SWAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SWAnimation
+//
+//  Created by YoonieMac on 6/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SWAnimation/SWAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWAnimationApp.swift
+//  SWAnimation
+//
+//  Created by YoonieMac on 6/12/26.
+//
+
+import SwiftUI
+
+@main
+struct SWAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
