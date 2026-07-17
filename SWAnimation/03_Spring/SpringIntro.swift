3da96e48b83a01aeef50f8c018bafc895ae48878
김도윤 <kdy0096@gmail.com>
2026-07-18 01:19:46 +0900

    SpringIntro

---
+++ SWAnimation/03_Spring/SpringIntro.swift
@@ -0,0 +1,18 @@
+//
+//  SpringIntro.swift
+//  SWAnimation
+//
+//  Created by yoonie on 7/18/26.
+//
+
+import SwiftUI
+
+struct SpringIntro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpringIntro()
+}
