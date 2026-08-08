fc7076b0c5528094b627a5deb31ebce79ed986b5
wannabemountainking <kdy0096@gmail.com>
2026-08-08 19:56:20 +0900

    ToastSystem Initial Commit

---
+++ SWAnimation/04_Transition/NotificationToastSystem.swift
@@ -0,0 +1,30 @@
+//
+//  NotificationToastSystem.swift
+//  SWAnimation
+//
+//  Created by YoonieMac on 8/8/26.
+//
+
+import SwiftUI
+import Observation
+
+// MARK: - 토스트 데이터 모델
+
+
+// MARK: - 토스트 메니저 (ViewModel)
+
+
+// MARK: - 메인 토스트 시스템 뷰
+struct NotificationToastSystem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - 개별 토스트 뷰 컴포넌트
+
+
+// MARK: - 프리뷰
+#Preview {
+    NotificationToastSystem()
+}
